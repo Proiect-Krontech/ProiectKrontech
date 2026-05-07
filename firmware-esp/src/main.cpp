962a17da4ad2f93d8f57a25213f9304c7b21256c
lazaralexandra29 <llazar.aalexandra@gmail.com>
2026-05-07 18:33:30 +0300

    chore: remove deprecated firmware-esp module

--- firmware-esp/src/main.cpp
+++
@@ -1,3 +0,0 @@
-//Codul este un bridge între Arduino și un server Node.js.
-//ESP32 citește JSON de pe UART, îl validează și îl trimite prin HTTP POST către un server din rețea, 
-//reconectând automat WiFi dacă pică.
