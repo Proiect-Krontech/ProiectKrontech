b3342e77c80b92e25bb373bface18d2184b97948
Ungureanu Bianca <biancaungureanu229@gmail.com>
2026-05-09 23:07:12 +0300

    chore: add shared secrets template

---
+++ firmware-arduino/src/Secrets.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// Replace these values with your local configuration.
+// Do not commit personal credentials.
+
+namespace SecretCfg {
+    constexpr const char* SSID = "YOUR_WIFI_SSID";
+    constexpr const char* PASSWORD = "YOUR_WIFI_PASSWORD";
+    constexpr const char* HOST = "YOUR_SERVER_HOST";
+}
